ee1c82a268e9f163672822d02ae9d26b176e192b
Dennis Chhay <d3chhay@gmail.com>
2018-12-07 18:35:24 -0800

    adds Throttler file replaces ThrottlerTests file

---
+++ Throttler/Code/Throttler.swift
@@ -0,0 +1,74 @@
+//
+//  Throttler.swift
+//  Throttler
+//
+//  Created by Dennis Chhay on 11/26/18.
+//  Copyright © 2018 Dennis Chhay. All rights reserved.
+//
+
+import Foundation
+
+@objc
+class Throttler: NSObject {
+    
+    var delay: TimeInterval = 0.5
+    private var dispatchQueue: DispatchQueue = DispatchQueue.main
+    private var dispatchBlock: (() -> Void) = {}
+    private var dispatchWork: DispatchWorkItem?
+    
+    convenience init(delay: TimeInterval = 0.5,
+                     dispatchQueue: DispatchQueue = DispatchQueue.main,
+                     dispatchBlock: @escaping () -> ()) {
+        self.init()
+        self.delay = delay
+        self.dispatchQueue = dispatchQueue
+        self.dispatchBlock = dispatchBlock
+    }
+    
+}
+
+// API
+extension Throttler {
+    
+    /**
+     Schedules the dispatch to run asynchronously after the delay time.
+     */
+    func throttle() {
+        if let work = dispatchWork {
+            work.cancel()
+        }
+        let work = DispatchWorkItem(block: dispatchBlock)
+        dispatchWork = work
+        let deadline = DispatchTime.now() + delay
+        dispatchQueue.asyncAfter(deadline: deadline, execute: work)
+    }
+    
+    /**
+     Triggers dispatch asynchronously.
+     */
+    func fire() {
+        if let work = dispatchWork,
+           !work.isCancelled {
+               work.perform()
+               work.cancel()
+               dispatchWork = nil
+        } else {
+            let work = DispatchWorkItem(block: dispatchBlock)
+            dispatchWork = work
+            let deadline = DispatchTime.now()
+            dispatchQueue.asyncAfter(deadline: deadline, execute: work)
+        }
+    }
+    
+    /**
+     Cancels any scheduled dispatch.
+     */
+    func cancel() {
+        guard let work = dispatchWork else {
+            return
+        }
+        work.cancel()
+        dispatchWork = nil
+    }
+    
+}
--- ThrottlerTests/ThrottlerTests.swift
+++ ThrottlerTests/ThrottlerTests.swift
@@ -2,7 +2,7 @@
 //  ThrottlerTests.swift
 //  ThrottlerTests
 //
-//  Created by Dennis Chhay on 12/7/18.
+//  Created by Dennis Chhay on 11/26/18.
 //  Copyright © 2018 Dennis Chhay. All rights reserved.
 //
 
@@ -11,26 +11,70 @@ import XCTest
 
 class ThrottlerTests: XCTestCase {
     
+    var throttler: Throttler?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        throttler = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testFire() {
+        let delay: TimeInterval = 1.5
+        let dispatchQueue = DispatchQueue.main
+        let expectation = XCTestExpectation(description: "\(#function)")
+        throttler = Throttler(delay: delay, dispatchQueue: dispatchQueue) {
+            expectation.fulfill()
+        }
+        throttler?.fire()
+        
+        let timeout: TimeInterval = 1.0
+        wait(for: [expectation], timeout: timeout)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testThrottle() {
+        let delay: TimeInterval = 0.5
+        let dispatchQueue = DispatchQueue.main
+        let expectation = XCTestExpectation(description: "\(#function)")
+        throttler = Throttler(delay: delay, dispatchQueue: dispatchQueue) {
+            expectation.fulfill()
         }
+        throttler?.throttle()
+        
+        let timeout: TimeInterval = 1.0
+        wait(for: [expectation], timeout: timeout)
+    }
+    
+    
+    func testThrottleTime() {
+        let delay: TimeInterval = 2.0
+        let secondThrottleWait = delay * 0.90
+        let dispatchQueue = DispatchQueue.main
+        let expectation = XCTestExpectation(description: "\(#function)")
+        let startTime = Date()
+        throttler = Throttler(delay: delay, dispatchQueue: dispatchQueue) {
+            let timeElapsed = Date().timeIntervalSince(startTime)
+            let expectedTimeElapsed = secondThrottleWait + delay
+            let percentDiff = abs(expectedTimeElapsed - timeElapsed) / expectedTimeElapsed
+            let tolerance: TimeInterval = 0.10
+            if percentDiff < tolerance  {
+                expectation.fulfill()
+            }
+        }
+        throttler?.throttle()
+        
+        // Second throttle
+        let deadline = DispatchTime.now() + secondThrottleWait
+        dispatchQueue.asyncAfter(deadline: deadline) { [weak self] in
+            self?.throttler?.throttle()
+        }
+
+        let padding: TimeInterval =  4.0
+        let timeout: TimeInterval = secondThrottleWait + delay + padding
+        wait(for: [expectation], timeout: timeout)
     }
     
 }
