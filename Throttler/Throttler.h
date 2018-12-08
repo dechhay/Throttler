793eb5c692d831fdbba19d5848745a899939227c
Dennis Chhay <d3chhay@gmail.com>
2018-12-07 18:24:08 -0800

    Initial Commit

---
+++ Throttler/Throttler.h
@@ -0,0 +1,19 @@
+//
+//  Throttler.h
+//  Throttler
+//
+//  Created by Dennis Chhay on 12/7/18.
+//  Copyright © 2018 Dennis Chhay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Throttler.
+FOUNDATION_EXPORT double ThrottlerVersionNumber;
+
+//! Project version string for Throttler.
+FOUNDATION_EXPORT const unsigned char ThrottlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Throttler/PublicHeader.h>
+
+
